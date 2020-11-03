776e03317d9cb21a3c4ac136606f2b37a8ada1c6
ramya <rammu02795@gmail.com>
2020-11-03 16:07:29 +0530

    base

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var currurl = window.location.pathname;
+var index = currurl.lastIndexOf("/") + 1;
+var filename = currurl.substr(index);
+var url_name=window.location.href;
+$(document).ready(function() {
+	
+});
